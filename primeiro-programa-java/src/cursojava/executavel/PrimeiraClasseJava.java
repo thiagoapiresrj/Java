7c2bd56e24bd4573b1803a8040b6a15cc8907fef
Administrator <Administrator@169.254.254.189>
2022-09-20 15:09:16 -0300

    alterando

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -2,7 +2,7 @@ package cursojava.executavel;
 
 public class PrimeiraClasseJava {
         public static void main(String [] args) {
-        	/*escreve no console*/
-        	System.out.println("Meu primeiro programa java!!!");
+        	/*escreve no console........*/
+        	System.out.println("Meu primeiro programa java!!!!!!");
         }
 }
