743b126e138264e222a946271daf8c16c1d67f9a
Administrator <Administrator@169.254.254.189>
2022-09-20 14:54:51 -0300

    Criação do projeto.

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,8 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+        public static void main(String [] args) {
+        	/*escreve no console*/
+        	System.out.println("Meu primeiro programa java!!!");
+        }
+}
